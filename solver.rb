632ffbaa5e05ed7adb383cdac1d8b388c52b43ff
xiranou <xiranou@gmail.com>
2015-10-04 19:24:54 -0400

    add comments about initial solve algo

--- solver.rb
+++ solver.rb
@@ -1,1 +1,17 @@
 sample = File.readlines(File.expand_path("./sample_unsolved.txt")).first.gsub("\n", "")
+
+class Solver
+  attr_accessor :sudoku_board
+
+  def initialize(string)
+    @sudoku_board = string.split("")
+  end
+
+  def solve!
+    # find 0
+    # insert guess according to the outcome from rules(vertical, horizonal, quad)
+    # store in back track array: {index: guess}
+    # repeat ^
+    # if no guess availble to current 0, back track up until guess available
+  end
+end
