f55fbef6f77f2830cb7ea9f04950f0e689c758be
xiranou <xiranou@gmail.com>
2015-10-04 19:31:44 -0400

    add find_next_empty

--- solver.rb
+++ solver.rb
@@ -9,9 +9,20 @@ class Solver
 
   def solve!
     # find 0
+    current = find_next_empty
     # insert guess according to the outcome from rules(vertical, horizonal, quad)
     # store in back track array: {index: guess}
     # repeat ^
     # if no guess availble to current 0, back track up until guess available
   end
+
+  def find_next_empty
+    sudoku_board.each_with_index do |element, index|
+      return {index => element} if element == '0'
+    end
+  end
 end
+
+p sample
+solver = Solver.new(sample)
+p solver.find_next_empty
