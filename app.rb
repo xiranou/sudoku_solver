e06fa8b1b7a485d4193acc950413b5e86e66e26f
xiranou <xiranou@gmail.com>
2015-10-06 17:17:39 -0400

    add post route for /solve

--- app.rb
+++ app.rb
@@ -4,3 +4,9 @@ get "/" do
   # @example = Solver.new(File.readlines(File.expand_path("./sample_unsolved.txt")).first).sudoku_board
   erb :home
 end
+
+post "/solve" do
+  content_type :json
+  @results = Solver.new(params[:data]).solve!
+  @results.to_json
+end
