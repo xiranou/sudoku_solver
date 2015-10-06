3028f6d5c1c219651f19f155316b108a5b8ec928
xiranou <xiranou@gmail.com>
2015-10-06 14:49:19 -0400

    comment out example code

--- app.rb
+++ app.rb
@@ -6,6 +6,6 @@ Bundler.require
 require './models/solver'
 
 get "/" do
-  @example = Solver.new(File.readlines(File.expand_path("./sample_unsolved.txt")).first).sudoku_board
+  # @example = Solver.new(File.readlines(File.expand_path("./sample_unsolved.txt")).first).sudoku_board
   erb :home
 end
