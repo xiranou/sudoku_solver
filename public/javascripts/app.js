1b4565b49670be5e9dc7a3af3eb33f7aa000354a
xiranou <xiranou@gmail.com>
2015-10-06 17:17:03 -0400

    initial ajax call setup

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,2 +1,29 @@
 $(function () {
+  $('body').on('submit', 'form.board', function(e) {
+    e.preventDefault();
+    var $form = $(e.target);
+    var url = $form.attr('action');
+    var data = $form.find("input[class='number-input']").map(function(index, elem) {
+      var val = $(elem).val();
+      if (val) {
+        return val;
+      } else {
+        return "0";
+      }
+    }).get().join("");
+
+    $.ajax({
+      url: url,
+      type: 'POST',
+      dataType: 'JSON',
+      data: {data: data},
+    })
+    .done(function(data) {
+      console.log(data);
+    })
+    .fail(function() {
+      console.log("error");
+    });
+
+  });
 });
