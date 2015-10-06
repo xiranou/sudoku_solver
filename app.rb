b74cfdf1fb8a8306ff76ae3c7b18291e922de78a
xiranou <xiranou@gmail.com>
2015-10-06 14:21:22 -0400

    use Bundler to require gems

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
-require 'sinatra'
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+# require 'sinatra'
+require './models/example'
 
 get "/" do
+  @example = Example.call
   erb :home
 end
